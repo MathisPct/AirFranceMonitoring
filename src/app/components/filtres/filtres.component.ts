4f1c4b02a8af49ebb81a606d33e7e2eef5659e19
Mathis Poncet-Crouzet <math712b@gmail.com>
2023-06-05 11:16:21 +0200

    feat: systeme de filtres Co-authored-by: NicolasGuruphat <nicolas.guruphat@gmail.com>

--- src/app/components/filtres/filtres.component.ts
+++ src/app/components/filtres/filtres.component.ts
@@ -1,27 +1,64 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { MAT_DATE_RANGE_SELECTION_STRATEGY } from '@angular/material/datepicker';
-import { FiveDayRangeSelectionStrategy } from 'src/app/date-adapter';
-import { IAeroport } from 'src/app/models/aeroport.model';
-import { AEROPORTS } from './../../constants/aeroport.constant';
+import {Component, EventEmitter, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {MAT_DATE_RANGE_SELECTION_STRATEGY} from '@angular/material/datepicker';
+import {FiveDayRangeSelectionStrategy} from 'src/app/date-adapter';
+import {IAeroport} from 'src/app/models/aeroport.model';
+import {AEROPORTS} from './../../constants/aeroport.constant';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {IFiltres} from "../../models/filtres.model";
+
+export interface FiltreFormModel extends FormGroup<{
+    aeroport: FormControl<IAeroport | null>;
+    debut: FormControl<number | null>;
+    fin: FormControl<number | null>;
+}> {
+
+}
 
 @Component({
-  selector: 'app-filtres',
-  templateUrl: './filtres.component.html',
-  styleUrls: ['./filtres.component.scss'],
-  providers: [
-    {
-      provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
-      useClass: FiveDayRangeSelectionStrategy,
-    },
-  ],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-filtres',
+    templateUrl: './filtres.component.html',
+    styleUrls: ['./filtres.component.scss'],
+    providers: [
+        {
+            provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+            useClass: FiveDayRangeSelectionStrategy,
+        },
+    ],
+    encapsulation: ViewEncapsulation.None
 })
-export class FiltresComponent {
+export class FiltresComponent implements OnInit {
+    /**
+     * La liste des aéroports disponibles est une constante,
+     * on n'utilise que les principaux aéroports français pour l'instant
+     */
+    aeroports: IAeroport[] = AEROPORTS;
 
-  /**
-   * La liste des aéroports disponibles est une constante,
-   * on n'utilise que les principaux aéroports français pour l'instant
-   */
-  aeroports: IAeroport[] = AEROPORTS;
+    form: FiltreFormModel = this._formBuilder.group({
+        aeroport: new FormControl<IAeroport | null>(null, Validators.required),
+        debut: new FormControl<number | null>(null, Validators.required),
+        fin: new FormControl<number | null>(null, Validators.required)
+    });
 
+    @Output() changeInput = new EventEmitter<IFiltres>();
+
+    constructor(private _formBuilder: FormBuilder) {
+    }
+    onSubmit() {
+        if(this.form.valid) {
+            const endDateValue: number = this.form.controls.fin.value ?? 0;
+            const beginDataValue: number = this.form.controls.debut.value ?? 0;
+            const selectedAeroport: IAeroport | null = this.form.controls.aeroport.value ?? null;
+            if(selectedAeroport) {
+                const dataToSend: IFiltres = {
+                    fin: new Date(endDateValue),
+                    debut: new Date(beginDataValue),
+                    aeroport: selectedAeroport
+                };
+                console.log(dataToSend);
+                this.changeInput.emit(dataToSend);
+            }
+        }
+    }
+    ngOnInit() {
+    }
 }
--- src/app/components/view-airfrance/view-airfrance.component.ts
+++ src/app/components/view-airfrance/view-airfrance.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import { IFiltres } from 'src/app/models/filtres.model';
 import { Vol } from 'src/app/models/vol.model';
 import { VolService } from '../../services/vol.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-view-airfrance',
   templateUrl: './view-airfrance.component.html',
   styleUrls: ['./view-airfrance.component.scss']
 })
-export class ViewAirFranceComponent {
+export class ViewAirFranceComponent implements OnDestroy {
 
   vols: Vol[] = [];
-
+  private _subscriptions: Subscription = new Subscription();
   constructor(private _volService: VolService) { }
 
   /**
@@ -21,7 +22,14 @@ export class ViewAirFranceComponent {
    * @param filtres récupérés depuis le composant enfant
    */
   onFiltresEvent(filtres: IFiltres): void {
-    // TODO
+    const subscription = this._volService.getVolsDepart(filtres.aeroport.icao, filtres.debut.getTime(), filtres.fin.getTime()).subscribe({
+      next: newVols => this.vols = newVols,
+      error: err => console.error(err)
+    });
+    this._subscriptions.add(subscription)
   }
 
+  ngOnDestroy(): void {
+      this._subscriptions.unsubscribe();
+  }
 }
